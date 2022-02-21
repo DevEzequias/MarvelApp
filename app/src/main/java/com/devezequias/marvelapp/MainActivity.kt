6f5a4c5cf61f7c414d8246b82758ae78b5a9b70f
Ezequias <ezequiasantos77@hotmail.com>
2022-02-21 18:30:52 -0400

    Add a Navigation

--- app/src/main/java/com/devezequias/marvelapp/MainActivity.kt
+++ app/src/main/java/com/devezequias/marvelapp/MainActivity.kt
@@ -2,14 +2,31 @@ package com.devezequias.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.devezequias.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottonNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
     }
 }
